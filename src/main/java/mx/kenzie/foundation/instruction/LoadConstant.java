aaf92abfd2dd0cf07586f0920ff4ac22cc9f0069
Moderocky <admin@moderocky.com>
2023-07-10 09:50:20 +0100

    Add number push to constants.

--- src/main/java/mx/kenzie/foundation/instruction/LoadConstant.java
+++ src/main/java/mx/kenzie/foundation/instruction/LoadConstant.java
@@ -1,12 +1,20 @@
 package mx.kenzie.foundation.instruction;
 
+import org.objectweb.asm.Opcodes;
+
 public class LoadConstant {
 
     LoadConstant() {
     }
 
     public <Result extends Number> Instruction.Input<Result> of(Result value) {
-        return visitor -> visitor.visitLdcInsn(value);
+        return visitor -> {
+            if (value instanceof Byte b) visitor.visitIntInsn(Opcodes.BIPUSH, b);
+            if (value instanceof Short s) visitor.visitIntInsn(Opcodes.SIPUSH, s);
+            if (value instanceof Integer i && i < Short.MAX_VALUE && i > Short.MIN_VALUE)
+                visitor.visitIntInsn(Opcodes.SIPUSH, i);
+            visitor.visitLdcInsn(value);
+        };
     }
 
     public Instruction.Input<Object> of(Object value) {
