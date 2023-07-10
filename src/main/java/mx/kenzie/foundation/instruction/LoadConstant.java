3b1ba0f6cc2a27cf7b1f693115fbc09837a08065
Moderocky <admin@moderocky.com>
2023-07-10 09:54:15 +0100

    Revert number constants.

--- src/main/java/mx/kenzie/foundation/instruction/LoadConstant.java
+++ src/main/java/mx/kenzie/foundation/instruction/LoadConstant.java
@@ -1,20 +1,12 @@
 package mx.kenzie.foundation.instruction;
 
-import org.objectweb.asm.Opcodes;
-
 public class LoadConstant {
 
     LoadConstant() {
     }
 
     public <Result extends Number> Instruction.Input<Result> of(Result value) {
-        return visitor -> {
-            if (value instanceof Byte b) visitor.visitIntInsn(Opcodes.BIPUSH, b);
-            if (value instanceof Short s) visitor.visitIntInsn(Opcodes.SIPUSH, s);
-            if (value instanceof Integer i && i < Short.MAX_VALUE && i > Short.MIN_VALUE)
-                visitor.visitIntInsn(Opcodes.SIPUSH, i);
-            visitor.visitLdcInsn(value);
-        };
+        return visitor -> visitor.visitLdcInsn(value); // todo can't check numbers but don't know why
     }
 
     public Instruction.Input<Object> of(Object value) {
