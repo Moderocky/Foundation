2f07e9b14b7a989271f800adaa5454a4fb5d355a
Moderocky <admin@moderocky.com>
2024-03-17 09:23:56 +0000

    Add synthetic attribute.

---
+++ src/main/java/mx/kenzie/foundation/assembler/attribute/Synthetic.java
@@ -0,0 +1,22 @@
+package mx.kenzie.foundation.assembler.attribute;
+
+import mx.kenzie.foundation.assembler.AttributeInfo;
+import mx.kenzie.foundation.assembler.U2;
+import mx.kenzie.foundation.assembler.U4;
+import mx.kenzie.foundation.assembler.UVec;
+import org.valross.constantine.RecordConstant;
+
+public record Synthetic(U2 attribute_name_index)
+    implements AttributeInfo, UVec, RecordConstant {
+
+    @Override
+    public U4 attribute_length() {
+        return U4.valueOf(0);
+    }
+
+    @Override
+    public UVec info() {
+        return this.attribute_length(); // re-use the zero value
+    }
+
+}
