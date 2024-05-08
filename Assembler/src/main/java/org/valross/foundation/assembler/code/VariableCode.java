99b6ee88f88122c2bc81d08bd6561eac2e5ba8a5
Moderocky <admin@moderocky.com>
2024-05-08 09:23:04 +0100

    Add type helper in variable codes.

--- Assembler/src/main/java/org/valross/foundation/assembler/code/VariableCode.java
+++ Assembler/src/main/java/org/valross/foundation/assembler/code/VariableCode.java
@@ -38,6 +38,24 @@ public abstract class VariableCode implements OpCode {
         return 2;
     }
 
+    public CodeElement var(Class<?> type, int slot) {
+        if (code < 54) { // 54 = ISTORE :)
+            if (type == int.class || type == boolean.class
+                || type == char.class || type == short.class || type == byte.class) return ILOAD.var(slot);
+            if (type == float.class) return FLOAD.var(slot);
+            if (type == long.class) return LLOAD.var(slot);
+            if (type == double.class) return FLOAD.var(slot);
+            return ALOAD.var(slot);
+        } else {
+            if (type == int.class || type == boolean.class
+                || type == char.class || type == short.class || type == byte.class) return ISTORE.var(slot);
+            if (type == float.class) return FSTORE.var(slot);
+            if (type == long.class) return LSTORE.var(slot);
+            if (type == double.class) return FSTORE.var(slot);
+            return ASTORE.var(slot);
+        }
+    }
+
     public CodeElement var(int slot) {
         U2.valueOf(slot);
         final int increment = switch (code) {
@@ -47,17 +65,17 @@ public abstract class VariableCode implements OpCode {
         };
         if (slot > 255)
             return increment > 0 ? CodeElement.notify(CodeElement.wide(CodeElement.fixed(code, (byte) (slot >>> 8),
-                                                                                         (byte) (slot))),
-                                                      StackNotifier.pushVariable(slot)) :
+                    (byte) (slot))),
+                StackNotifier.pushVariable(slot)) :
                 CodeElement.notify(CodeElement.wide(CodeElement.fixed(code, (byte) (slot >>> 8), (byte) (slot))),
-                                   builder -> builder.register()
-                                                     .put(slot, builder.stack()
-                                                                       .popSafe()));
+                    builder -> builder.register()
+                        .put(slot, builder.stack()
+                            .popSafe()));
         return increment > 0 ? CodeElement.notify(CodeElement.fixed(code, (byte) slot),
-                                                  StackNotifier.pushVariable(slot)) :
+            StackNotifier.pushVariable(slot)) :
             CodeElement.notify(CodeElement.fixed(code, (byte) slot), builder -> builder.register()
-                                                                                       .put(slot, builder.stack()
-                                                                                                         .popSafe()));
+                .put(slot, builder.stack()
+                    .popSafe()));
     }
 
     @Override
