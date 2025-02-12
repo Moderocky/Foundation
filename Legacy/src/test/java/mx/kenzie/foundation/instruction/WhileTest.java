187576e477ea3d7777a299bb5775bc9701244f7a
Andrey Vorobev <andrey.vorobev.aqa@gmail.com>
2025-02-12 23:19:10 +0300

    Refactoring WhileTest.java

--- Legacy/src/test/java/mx/kenzie/foundation/instruction/WhileTest.java
+++ Legacy/src/test/java/mx/kenzie/foundation/instruction/WhileTest.java
@@ -14,24 +14,36 @@ import static mx.kenzie.foundation.instruction.Instruction.Operator.*;
 
 public class WhileTest extends FoundationTest {
 
+    /**
+     * Tests a while loop that increments a variable until it reaches 10.
+     * The method initializes a variable to 0, then uses a while loop to increment it
+     * until the value is no longer less than 10. Finally, it returns true if the
+     * variable equals 10.
+     */
     @Test
-    public void testCheck() {
-        final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testCheck");
+    public void testWhileLoopIncrementUntilTen() {
+        final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testWhileLoopIncrementUntilTen");
         method.line(STORE_VAR.intValue(0, ZERO));
-        final Block check;
-        method.line(check = WHILE.check(COMPARE.ints(LOAD_VAR.intValue(0), LESS, CONSTANT.of(10))));
-        check.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
+        final Block whileBlock;
+        method.line(whileBlock = WHILE.check(COMPARE.ints(LOAD_VAR.intValue(0), LESS, CONSTANT.of(10))));
+        whileBlock.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
         method.line(RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, CONSTANT.of(10))));
         this.thing.add(method);
     }
 
+    /**
+     * Tests a do-while loop that increments a variable and checks the condition afterward.
+     * The method initializes a variable to 0, then uses a do-while loop to increment it
+     * at least once, continuing as long as the variable is greater than 10. Finally,
+     * it returns true if the variable equals 1.
+     */
     @Test
-    public void testDoWhile() {
-        final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testDoWhile");
+    public void testDoWhileLoopIncrementAndCheckCondition() {
+        final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testDoWhileLoopIncrementAndCheckCondition");
         method.line(STORE_VAR.intValue(0, ZERO));
-        final Block check;
-        method.line(check = WHILE.doWhile(COMPARE.ints(LOAD_VAR.intValue(0), GREATER, CONSTANT.of(10))));
-        check.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
+        final Block doWhileBlock;
+        method.line(doWhileBlock = WHILE.doWhile(COMPARE.ints(LOAD_VAR.intValue(0), GREATER, CONSTANT.of(10))));
+        doWhileBlock.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
         method.line(RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, ONE)));
         this.thing.add(method);
     }
