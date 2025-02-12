474d8f5771225ac93acf0be90662f04be2932d2b
Andrey Vorobev <andrey.vorobev.aqa@gmail.com>
2025-02-12 23:29:01 +0300

    Refactoring WhileTest.java

--- Legacy/src/test/java/mx/kenzie/foundation/instruction/WhileTest.java
+++ Legacy/src/test/java/mx/kenzie/foundation/instruction/WhileTest.java
@@ -14,6 +14,13 @@ import static mx.kenzie.foundation.instruction.Instruction.Operator.*;
 
 public class WhileTest extends FoundationTest {
 
+    // Constants for integer values
+    private static final int INITIAL_VALUE = 0;
+    private static final int LOOP_INCREMENT = 1;
+    private static final int LOOP_TERMINATION_CONDITION = 10;
+    private static final int EXPECTED_FINAL_VALUE = 10;
+    private static final int DO_WHILE_EXPECTED_VALUE = 1;
+
     /**
      * Tests a while loop that increments a variable until it reaches 10.
      * The method initializes a variable to 0, then uses a while loop to increment it
@@ -23,11 +30,22 @@ public class WhileTest extends FoundationTest {
     @Test
     public void testWhileLoopIncrementUntilTen() {
         final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testWhileLoopIncrementUntilTen");
-        method.line(STORE_VAR.intValue(0, ZERO));
+
+        method.line(STORE_VAR.intValue(0, CONSTANT.of(INITIAL_VALUE)));
+
         final Block whileBlock;
-        method.line(whileBlock = WHILE.check(COMPARE.ints(LOAD_VAR.intValue(0), LESS, CONSTANT.of(10))));
-        whileBlock.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
-        method.line(RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, CONSTANT.of(10))));
+        method.line(whileBlock = WHILE.check(
+                COMPARE.ints(LOAD_VAR.intValue(0), LESS, CONSTANT.of(LOOP_TERMINATION_CONDITION))
+        ));
+
+        whileBlock.line(
+                STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, CONSTANT.of(LOOP_INCREMENT)))
+        );
+
+        method.line(
+                RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, CONSTANT.of(EXPECTED_FINAL_VALUE))
+                ));
+
         this.thing.add(method);
     }
 
@@ -40,11 +58,22 @@ public class WhileTest extends FoundationTest {
     @Test
     public void testDoWhileLoopIncrementAndCheckCondition() {
         final PreMethod method = new PreMethod(PUBLIC, STATIC, BOOLEAN, "testDoWhileLoopIncrementAndCheckCondition");
-        method.line(STORE_VAR.intValue(0, ZERO));
+
+        method.line(STORE_VAR.intValue(0, CONSTANT.of(INITIAL_VALUE)));
+
         final Block doWhileBlock;
-        method.line(doWhileBlock = WHILE.doWhile(COMPARE.ints(LOAD_VAR.intValue(0), GREATER, CONSTANT.of(10))));
-        doWhileBlock.line(STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, ONE)));
-        method.line(RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, ONE)));
+        method.line(doWhileBlock = WHILE.doWhile(
+                COMPARE.ints(LOAD_VAR.intValue(0), GREATER, CONSTANT.of(LOOP_TERMINATION_CONDITION))
+        ));
+
+        doWhileBlock.line(
+                STORE_VAR.intValue(0, SUM.ints(LOAD_VAR.intValue(0), PLUS, CONSTANT.of(LOOP_INCREMENT)))
+        );
+
+        method.line(
+                RETURN.intValue(COMPARE.ints(LOAD_VAR.intValue(0), EQ, CONSTANT.of(DO_WHILE_EXPECTED_VALUE))
+                ));
+
         this.thing.add(method);
     }
 
